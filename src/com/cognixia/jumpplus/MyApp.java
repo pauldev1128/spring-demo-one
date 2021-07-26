ba554ab1a0f265dfaf146bea62d5713f28274052
Paul Cho <47371078+pauldev1128@users.noreply.github.com>
2021-07-26 12:57:41 -0700

    created Baseball coach and MyApp

---
+++ src/com/cognixia/jumpplus/MyApp.java
@@ -0,0 +1,13 @@
+package com.cognixia.jumpplus;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// create the obj
+		BaseballCoach theCoach = new BaseballCoach();
+		
+		//use the obj
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
